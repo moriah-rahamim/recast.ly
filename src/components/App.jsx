b245815c79bd6d331ae07f1b92925fc0acf38bdb
moriah-rahamim <moriahfaye@gmail.com>
2018-01-31 17:32:44 -0800

    make video player switch after clicking a new video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,58 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: window.exampleVideoData,
+      nowPlaying: window.exampleVideoData[0]
+    };
+
+  }
+
+  playVideo(video) {
+    this.setState({
+      nowPlaying: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.nowPlaying} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} playVideo={this.playVideo.bind(this)} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={window.exampleVideoData[0]} />
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={window.exampleVideoData} />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <Search />
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer video={window.exampleVideoData[0]} />
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos={window.exampleVideoData} />
+//       </div>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
